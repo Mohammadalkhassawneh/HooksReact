c82d84804bec03010cd4c204e14d4efcc0f67e26
Mohammadalkhassawneh <90184941+Mohammadalkhassawneh@users.noreply.github.com>
2022-02-06 13:09:12 +0200

    reactstate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  //write JS here
+  const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const incrementer = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const toggler = () => {
+    setToggle((prev) => !prev);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>State using functional component</h1>
+      <h1 className={toggle ? "active" : ""}>Hello React</h1>
+      <h2>Counter {counter}</h2>
+
+      <button onClick={incrementer}>Counter</button>
+      <button onClick={toggler}>Toggle</button>
     </div>
   );
 }
